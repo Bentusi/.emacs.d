8f9835f904368615a43c266646adf0c25efd0d3e
JIANG Wei <jiangwei.chn@gmail.com>
2019-05-06 21:58:51 +0800

    更新ELPA库

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 安装时将所有文件放置到~/.emacs.d目录下。
 并且安装fonts目录下所有字体到系统。
-编程字体默认Source Code。汉字默认微软雅黑。
+编程字体默认Source Code。汉字Windows默认微软雅黑。
 
-我是在Windows 10下使用Emacs，配合了cygwin。要求版本号>=26.1。
+我是在Windows 10、MacOS、Ubuntu下使用Emacs。要求版本号>=26.1。
 
 gitlab: https://gitlab.com/bentusi/beemacs
 
---
+++ elpa/promise-20190415.650/promise-pkg.el
@@ -0,0 +1,13 @@
+(define-package "promise" "20190415.650" "Promises/A+"
+  '((emacs "25")
+    (async "1.9"))
+  :keywords
+  '("async" "promise" "convenience")
+  :authors
+  '(("chuntaro" . "chuntaro@sakura-games.jp"))
+  :maintainer
+  '("chuntaro" . "chuntaro@sakura-games.jp")
+  :url "https://github.com/chuntaro/emacs-promise")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
