4a78814c8c6cc715cf2f0b0d8618fb52bc26872e
JIANG Wei <jiangwei.chn@gmail.com>
2020-03-12 22:55:48 +0800

     更新插件0312

---
+++ elpa/eshell-prompt-extras-20200310.809/eshell-prompt-extras-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eshell-prompt-extras" "20200310.809" "Display extra information for your eshell prompt." '((emacs "25")) :stars nil :commit "6a30813893e04ab8c0793757cdb2ff9873ab0c56" :keywords '("eshell" "prompt") :authors '(("zwild" . "judezhao@outlook.com")) :maintainer '("Chunyang Xu" . "mail@xuchunyang.me") :url "https://github.com/zwild/eshell-prompt-extras")
--- elpa/hungry-delete-20200307.2000/hungry-delete-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hungry-delete" "20200307.2000" "hungry delete minor mode" 'nil :commit "fe773607ee18c76f34831999a635d46a8a188264" :authors '(("Nathaniel Flath" . "flat0103@gmail.com")) :maintainer '("Nathaniel Flath" . "flat0103@gmail.com") :url "http://github.com/nflath/hungry-delete")
