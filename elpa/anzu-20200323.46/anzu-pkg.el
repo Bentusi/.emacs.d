43a85035c06a86897c93a98a104447358f326757
JIANG Wei <jiangwei.chn@gmail.com>
2020-03-24 21:08:06 +0800

    更新插件0324

---
+++ elpa/anzu-20200323.46/anzu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "anzu" "20200323.46" "Show number of matches in mode-line while searching" '((emacs "24.3")) :stars nil :commit "e4c2728734fba74a521a3296aa9c0443992da18a" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com") :url "https://github.com/emacsorphanage/anzu")
